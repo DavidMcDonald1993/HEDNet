c46fe4a39aeacbbbc0cd6c3ee72b4ceecbc202ae
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-11-25 13:50:46 +0000

    cleanup of bash directory and added evauation scripts for g2g and hednet

--- bash/evaluate/evaluate_reconstruction.sh
+++ bash/evaluate/evaluate_reconstruction.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-#SBATCH --job-name=HEDNETevaluateLP
-#SBATCH --output=HEDNETevaluateLP_%A_%a.out
-#SBATCH --error=HEDNETevaluateLP_%A_%a.err
-#SBATCH --array=0-149
-#SBATCH --time=05:00
+#SBATCH --job-name=HEDNETevaluateRECON
+#SBATCH --output=HEDNETevaluateRECON_%A_%a.out
+#SBATCH --error=HEDNETevaluateRECON_%A_%a.err
+#SBATCH --array=0-749
+#SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=5G
+#SBATCH --mem=30G
 
-datasets=({00..29})
+datasets=({cora_ml,citeseer,pubmed,wiki_vote,email})
 dims=(2 5 10 25 50)
-# seeds=({0..29})
-seeds=(0)
-exp=lp_experiment
+seeds=({0..29})
+exp=recon_experiment
 
 num_datasets=${#datasets[@]}
 num_dims=${#dims[@]}
@@ -26,18 +25,17 @@ dataset=${datasets[$dataset_id]}
 dim=${dims[$dim_id]}
 seed=${seeds[$seed_id]}
 
-data_dir=datasets/synthetic_scale_free/${dataset}
+data_dir=datasets/${dataset}
 edgelist=${data_dir}/edgelist.tsv
-embedding_dir=embeddings/synthetic_scale_free/${dataset}/${exp}
-output=edgelists/synthetic_scale_free/${dataset}
+embedding_dir=embeddings/${dataset}/${exp}
 
 test_results=$(printf \
-    "test_results/synthetic_scale_free/${exp}/dim=%03d/HEDNet/" ${dim})
+    "test_results/${dataset}/${exp}/dim=%03d/HEDNet/" ${dim})
 embedding_dir=$(printf \
-    "${embedding_dir}/seed=%03d/dim=%03d" ${seed} ${dim})
+    "${embedding_dir}/seed=%03d/dim=%03d/" ${seed} ${dim})
 echo ${embedding_dir}
 
-args=$(echo --edgelist ${edgelist} --output ${output} --dist_fn klh \
+args=$(echo --edgelist ${edgelist} --dist_fn klh \
     --embedding ${embedding_dir} --seed ${dataset} \
     --test-results-dir ${test_results})
 echo ${args}
@@ -46,4 +44,4 @@ module purge
 module load bluebear
 module load apps/python3/3.5.2
 
-python evaluate_lp.py ${args}
+python evaluate_reconstruction.py ${args}
--- bash/evaluate/evaluate_reconstruction_g2g.sh
+++ bash/evaluate/evaluate_reconstruction_g2g.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=G2GevaluateLP
-#SBATCH --output=G2GevaluateLP_%A_%a.out
-#SBATCH --error=G2GevaluateLP_%A_%a.err
+#SBATCH --job-name=G2GevaluateRECON
+#SBATCH --output=G2GevaluateRECON_%A_%a.out
+#SBATCH --error=G2GevaluateRECON_%A_%a.err
 #SBATCH --array=0-749
-#SBATCH --time=05:00
+#SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=5G
+#SBATCH --mem=20G
 
 scales=(False)
-datasets=({00..29})
+datasets=({cora_ml,citeseer,pubmed,wiki_vote,email})
 dims=(2 5 10 25 50)
-seeds=(0)
-ks=(02 03 04 05 06)
-exp=lp_experiment
+seeds=({0..29})
+ks=(03)
+exp=recon_experiment
 
 num_scales=${#scales[@]}
 num_datasets=${#datasets[@]}
@@ -32,18 +32,19 @@ dataset=${datasets[$dataset_id]}
 dim=${dims[$dim_id]}
 seed=${seeds[$seed_id]}
 k=${ks[k_id]}
-data_dir=datasets/synthetic_scale_free/${dataset}
+
+data_dir=datasets/${dataset}
 edgelist=${data_dir}/edgelist.tsv
-embedding_dir=../graph2gauss/embeddings/synthetic_scale_free/${dataset}/${exp}/scale=${scale}/k=${k}
-output=edgelists/synthetic_scale_free/${dataset}
+embedding_dir=../grapg2gauss/embeddings/${dataset}/recon_experiment
+embedding_dir=$(printf "${embedding_dir}/scale=${scale}/k=${k}/seed=%03d/dim=%03d/" ${seed} ${dim})
 
 test_results=$(printf \
-    "test_results/synthetic_scale_free/${exp}/dim=%03d/g2g_k=${k}/" ${dim})
+    "test_results/${dataset}/${exp}/dim=%03d/g2g_k=${k}/" ${dim})
 embedding_dir=$(printf \
-    "${embedding_dir}/seed=%03d/dim=%03d" ${seed} ${dim})
+    "${embedding_dir}/seed=%03d/dim=%03d/" ${seed} ${dim})
 echo ${embedding_dir}
 
-args=$(echo --edgelist ${edgelist} --output ${output} --dist_fn kle \
+args=$(echo --edgelist ${edgelist} --dist_fn kle \
     --embedding ${embedding_dir} --seed ${dataset} \
     --test-results-dir ${test_results})
 echo ${args}
@@ -52,4 +53,4 @@ module purge
 module load bluebear
 module load apps/python3/3.5.2
 
-python evaluate_lp.py ${args}
+python evaluate_reconstruction.py ${args}
